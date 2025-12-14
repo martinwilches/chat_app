385de83b5ba0b71427c18a3ca3d104ff9812cb93
Martin Wilches <martin.devsoft@gmail.com>
2025-12-13 22:38:32 -0500

    Instalacion inicial de dependencias y creacion del server

---
+++ backend/README.md
@@ -0,0 +1,18 @@
+# Chat App Backend
+
+## Packages
+
+- express
+- cors
+- cookie-parser
+- dotenv
+- bcryptjs
+- jsonwebtoken
+- mongoose
+- redis
+- socket.io
+- cookie
+
+## Development Packages
+
+- nodemon
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import http from 'http'
+
+dotenv.config()
+
+const app = express()
+const httpServer = http.createServer(app)
+
+app.use(cors({
+    // habilitar CORS para recibir peticiones desde el lado del front
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true
+}))
+
+app.use(cookieParser())
+
+try {
+    const PORT = process.env.PORT || 4000
+    httpServer.listen(PORT, () => {
+        console.log(`Server running on port: ${PORT}`)
+    })
+} catch (error) {
+    console.log('The server failed to start: ', error)
+    process.exit(1)
+}
