5cc0255087641d358baf818d586e0d66b1620b08
Martin Wilches <martin.devsoft@gmail.com>
2026-01-02 21:09:13 -0500

    Definicion de componente para la vista de chat y sidebar

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,5 +1,19 @@
+import ChatWindow from '../../components/ChatWindow/ChatWindow'
+import Sidebar from '../../components/Sidebar/Sidebar'
+
 const Chat: React.FC = () => {
-    return <>This a chat page</>
+    return (
+        <>
+            <div className="flex min-h-screen bg-gray-100">
+                <div className="w-full sm:w-1/3 max-w-114 min-h-screen">
+                    <Sidebar />
+                </div>
+                <div className="hidden flex-1 min-h-screen sm:flex">
+                    <ChatWindow />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Chat
