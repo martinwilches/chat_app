e4c2f4f2ff2e1e1cbd92ba8556b22148fa4424b3
Martin Wilches <martin.devsoft@gmail.com>
2026-01-06 13:50:26 -0500

    QueryClient descripcion

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,17 +6,21 @@ import App from './App.tsx'
 // la aplicacion debe envolverse con el componente <BrowserRouter> para habilitar el enrutamiento
 import { BrowserRouter } from 'react-router'
 
-// `TanStack Query` permite manejar datos remotos (API's) de forma eficiente, sin usar `useState` ni `useEffect`
+/**
+* `QueryCliente` permite gestionar datos remotos (API's) de forma eficiente, sin usar `useState` ni `useEffect`
+* `QueryCliente` es el administrador global de:
+*     - cache de datos (reutiliza datos entre componentes)
+*     - estados automaticos (isLoading, isError, data, isFetching)
+*     - reintentos y refetch
+*     - sincronizacion (si 2 componentes piden lo mismos, se hace una sola llamada y ambos reciben los datos)
+*/
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // configurar el `QueryClient`
 const queryClient = new QueryClient({
     defaultOptions: {
-        queries: { // estas opciones por defecto solo afectan las consultas de lectura (useQuery)
-            /**
-             * Por defecto `TanStack` al cambiar de pestaña y luego volver a la app, vuelve a pedir los datos a la API
-             * Con `refetchOnWindowFocus: false`, los datos se quedan en cache tal como estan, no se vuelve a consultar a la API
-            */
+        queries: {
+            // `false` los datos se quedan en cache tal como estan, no se vuelven a consultar a la API
             refetchOnWindowFocus: false
         }
     }
